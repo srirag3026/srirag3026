dad44a8ae0ea9055a5f16c76fcffdb7dc1bb5b87
srividya.raghuraman05@gmail.com <srividya.raghuraman05@gmail.com>
2023-02-08 02:43:59 +0000

    commit

---
+++ models/marts/core/int_orders_pivoted.sql
@@ -0,0 +1,24 @@
+with
+    payments as (select * from {{ ref("stg_payments") }}),
+    pivoted as (
+        select
+            order_id,
+            sum(
+                case when payment_method = 'bank_transfer' then amount else 0 end
+            ) as bank_transfer_amount,
+            sum(
+                case when payment_method = 'Coupon' then amount else 0 end
+            ) as coupon_amount,
+            sum(
+                case when payment_method = 'credit_card' then amount else 0 end
+            ) as credit_card_amount,
+            sum(
+                case when payment_method = 'gift_card' then amount else 0 end
+            ) as gift_card_amount
+        from payments
+        where status = 'success'
+        group by 1
+    )
+
+select *
+from pivoted
