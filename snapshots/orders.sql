a710c569c7a4e3fed1057c9f7668ad8b8ef0f68a
srividya.raghuraman05@gmail.com <srividya.raghuraman05@gmail.com>
2023-02-08 03:41:41 +0000

    snapshot

---
+++ snapshots/orders.sql
@@ -0,0 +1,16 @@
+{% snapshot orders_snapshot %}
+
+{{
+    config(
+      target_database='analytics',
+      target_schema='snapshots',
+      unique_key='id',
+
+      strategy='timestamp',
+      updated_at='updated_at',
+    )
+}}
+
+select * from {{ source('jaffle_shop', 'orders') }}
+
+{% endsnapshot %}
